9e00cc2d33085c45d89007aeed37a4803eecfcb0
thxnder <rrln.1810@gmail.com>
2018-04-25 10:48:21 -0500

    Add files via upload

---
+++ constantes.js
@@ -0,0 +1,12 @@
+/* global moment:true */
+sap.ui.define([
+    
+], function() {
+    "use strict";
+    return {
+        URLSOAP: "http://140.20.0.214:8080/sap/bc/srt/rfc/sap/",
+        ClientSOAP: "300",
+        usernameSOAP: "TTAPIA",
+        passwordSOAP: "Seidor2017"
+    };
+});
---
+++ controladores.js
@@ -0,0 +1,12 @@
+/* global moment:true */
+sap.ui.define([
+    
+], function() {
+    "use strict";
+    return {
+        AppController: null,
+        LoginController: null,
+        MenuController: null,
+        DlgErroresController: null
+    };
+});
